22a3a969aaffa3d080ef2feabd1bb8c02400c5e0
Vijay <dattada.vijay@gmail.com>
2026-02-08 12:02:03 -0800

    job updated

--- src/data_ingestions.py
+++ src/data_ingestions.py
@@ -1,10 +1,17 @@
 # Databricks notebook source
 
-df = (spark.readStream.format("cloudFiles")
-      .option("cloudFiles.format", "csv")
-      .option("cloudFiles.schemaLocation", "/Volumes/stateful_processor/default/raw_data/SchemaLocation/")
-      .option("cloudFiles.schemaEvolutionMode", "addNewColumns")
-      .load("/Volumes/stateful_processor/default/raw_data/*.csv"))
+from pyspark.sql.functions import col, to_timestamp
+
+df = (
+    spark.readStream.format("cloudFiles")
+    .option("cloudFiles.format", "csv")
+    .option("cloudFiles.schemaLocation", "/Volumes/stateful_processor/default/raw_data/SchemaLocation/")
+    .option("cloudFiles.schemaEvolutionMode", "addNewColumns")
+    .load("/Volumes/stateful_processor/default/raw_data/*.csv")
+    .withColumn("amount", col("amount").cast("int"))
+    .withColumn("event_time", to_timestamp(col("event_time"), "yyyy-MM-dd'T'HH:mm:ss"))
+)
+
 
 # COMMAND ----------
 df.writeStream.format("delta").option("checkpointLocation", "/Volumes/stateful_processor/default/raw_data/CheckPoint/").outputMode("append").trigger(availableNow=True).toTable("stateful_processor.default.streaming_query")
